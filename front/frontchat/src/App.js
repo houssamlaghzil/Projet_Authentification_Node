984145f0a39f4f63cda34efda72f8c894ce5e2e1
Resxt <55228336+Resxt@users.noreply.github.com>
2021-12-01 15:05:06 +0100

    Login page
    Added login page with Halfmoon style
    Added register page

--- front/frontchat/src/App.js
+++ front/frontchat/src/App.js
@@ -2,19 +2,26 @@ import React from "react";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 import LoginPage from "./Login/Login";
+import RegisterPage from "./Register/Register";
 import ChatPage from "./Chat/Chat";
+
+import './App.css';
 
 export default function App() {
     return (
+
         <Router>
                 <Switch>
-                    <Route path="/chat">
-                        <Chat />
-                    </Route>
                     <Route path="/login">
                         <Login />
                     </Route>
-                    <Route path="/">
+                    <Route path="/register">
+                        <Register />
+                    </Route>
+                    <Route path="/chat">
+                        <Chat />
+                    </Route>
+                    <Route exactPath="/">
                         <Login />
                     </Route>
                 </Switch>
@@ -22,10 +29,14 @@ export default function App() {
     );
 }
 
-function Chat() {
-    return ChatPage();
-}
-
 function Login() {
     return LoginPage();
+}
+
+function Register() {
+    return RegisterPage();
+}
+
+function Chat() {
+    return ChatPage();
 }
