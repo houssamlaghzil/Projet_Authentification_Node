0d371746e2c1672fd8d02067b2ee33abe104f9bc
Resxt <55228336+Resxt@users.noreply.github.com>
2021-12-01 15:12:16 +0100

    Register page

--- front/frontchat/src/Register/Register.js
+++ front/frontchat/src/Register/Register.js
@@ -1,7 +1,46 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 export default function Register() {
     return(
-        <h2>OUI</h2>
+        <>
+            <head>
+                <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+                <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
+                <meta name="viewport" content="width=device-width" />
+
+            </head>
+            <body className="with-custom-webkit-scrollbars with-custom-css-scrollbars" data-dm-shortcut-enabled="true" data-set-preferred-mode-onload="true">
+            <div className="page-wrapper">
+
+                <div className="sticky-alerts"/>
+
+                <div className="content-wrapper">
+                    <form action="register" method="post" className="form-inline w-400 mw-full">
+                        <div className="form-group">
+                            <input type="email" className="form-control" placeholder="Email" id="email" required="required" />
+                        </div>
+                        <div className="form-group">
+                            <input type="text" className="form-control" placeholder="Username" id="username" required="required" />
+                        </div>
+                        <div className="form-group">
+                            <input type="password" className="form-control" placeholder="Password" id="password" required="required" />
+                        </div>
+                        <div className="form-group">
+                            <input type="password" className="form-control" placeholder="Confirm your password" id="password_confirm" required="required" />
+                        </div>
+                        <input type="hidden" id="user_type" />
+                        <div className="form-group mb-0">
+                            <div className="custom-control">
+                                <Link to="login" className="btn" type="button">Back to login</Link>
+                            </div>
+                            <input type="submit" className="btn btn-primary ml-auto" value="Register" />
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+            </body>
+        </>
     )
 }
